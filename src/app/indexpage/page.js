c71ed4a801624cd455c734467938a00d1b0ce952
ikiK <kiki.ikik@gmail.com>
2024-06-06 18:30:43 +0200

    ffirst good navigation

---
+++ src/app/indexpage/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <div>
+        <h1>Hello, Index page!</h1>
+        <Link href="/">Index</Link>
+      </div>
+    </div >
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Hello, Login page!</h1>
+    <div>
+      <div className={[styles.header, styles.center].join(" ")}>
+        <h1>Hello, Login page!</h1>
+        <Link href="/indexpage">Log in</Link>
+      </div>
+    </div >
   );
 }
