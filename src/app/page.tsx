a141ca46740e5a6917490b2da80746e734fe8ae2
ikiK <kiki.ikik@gmail.com>
2024-06-07 18:35:27 +0200

    added toast and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,17 @@ import Link from "next/link";
 import { useRouter } from 'next/navigation'
 import { FormEvent } from 'react'
 import data from "./users.json"
+import toast, { Toaster } from 'react-hot-toast';
+import logo from './logo.png'
+import { useState } from 'react'
 
 export default function Home() {
   const router = useRouter()
+
+  const imageStyle = {
+    backgroundColor: "white"
+  }
+
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -26,21 +34,32 @@ export default function Home() {
       password: password
     }
 
+
+    let t = false
     Object.values(data)[0].forEach(e => {
-      console.log(JSON.stringify(e) === JSON.stringify(formDataObj))
-      JSON.stringify(e) === JSON.stringify(formDataObj) ? router.push('/indexpage') : null
+      // console.log(JSON.stringify(e) === JSON.stringify(formDataObj))
+      if (JSON.stringify(e) === JSON.stringify(formDataObj)) t = true
     })
 
+    !t ? toast.error('Krivi podaci', {
+      toastId: 'success1',
+    }) : router.push('/indexpage')
   }
 
 
   return (
     <div>
       <div className="header center">
-        <h1>Hello, Index/Login page!</h1>
-        <Link href="/indexpage">Log in</Link>
-
+        <Image
+          width={151}
+          height={48}
+          src={logo}
+          alt="Logo"
+          style={imageStyle}
+        />
+        <h1>Tools</h1>
       </div>
+
       <div className="center2">
         <form onSubmit={handleSubmit}>
           <input type="text" name="name" placeholder="User" required />
@@ -48,6 +67,7 @@ export default function Home() {
           <button type="submit">Login</button>
         </form>
       </div>
+      <Toaster position="top-right" />
     </div >
   );
 }
