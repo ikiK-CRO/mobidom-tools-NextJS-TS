b7f66ab9b56412844da8923b27262138e518c332
ikiK <kiki.ikik@gmail.com>
2024-06-08 21:30:56 +0200

    added ts function to get data from texboxes

--- src/app/indexpage/tool1/page.tsx
+++ src/app/indexpage/tool1/page.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 import TextareaAutosize from 'react-textarea-autosize';
 
 export default function Page() {
+
+  let gettingValue = function gettingValue(data: string) {
+    console.log(data)
+  }
+
   return (
     <div>
       <div className="header center padd">
@@ -10,12 +15,12 @@ export default function Page() {
         <Link href="/indexpage">NAZAD</Link><br />
       </div>
       <div className="fullFlexContainer">
-        <TextareaAutosize placeholder="Kupac kolumna A" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
-        <TextareaAutosize placeholder="Kupac kolumna B" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
-        <TextareaAutosize placeholder="Kupac kolumna c" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
-        <TextareaAutosize placeholder="Dobavljač kolumna A" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
-        <TextareaAutosize placeholder="Dobavljač kolumna B" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
-        <TextareaAutosize placeholder="Dobavljač kolumna C" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
+        <TextareaAutosize placeholder="Kupac kolumna A" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value)} />
+        <TextareaAutosize placeholder="Kupac kolumna B" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value)} />
+        <TextareaAutosize placeholder="Kupac kolumna c" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value)} />
+        <TextareaAutosize placeholder="Dobavljač kolumna A" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value)} />
+        <TextareaAutosize placeholder="Dobavljač kolumna B" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value)} />
+        <TextareaAutosize placeholder="Dobavljač kolumna C" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value)} />
       </div>
     </div >
   );
