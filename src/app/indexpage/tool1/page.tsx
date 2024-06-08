5e65cf4ec3b97ce38c5590418d067a1a60909655
ikiK <kiki.ikik@gmail.com>
2024-06-08 21:16:20 +0200

    added full texboxes on change

--- src/app/indexpage/tool1/page.tsx
+++ src/app/indexpage/tool1/page.tsx
@@ -1,13 +1,22 @@
+"use client"
 import Link from "next/link";
+import TextareaAutosize from 'react-textarea-autosize';
 
 export default function Page() {
   return (
-    <div className="">
+    <div>
       <div className="header center padd">
-        <h1>TOOL 1</h1>
+        <h1>USPOREDBA 1</h1>
         <Link href="/indexpage">NAZAD</Link><br />
       </div>
-      test2
+      <div className="fullFlexContainer">
+        <TextareaAutosize placeholder="Kupac kolumna A" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
+        <TextareaAutosize placeholder="Kupac kolumna B" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
+        <TextareaAutosize placeholder="Kupac kolumna c" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
+        <TextareaAutosize placeholder="Dobavljač kolumna A" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
+        <TextareaAutosize placeholder="Dobavljač kolumna B" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
+        <TextareaAutosize placeholder="Dobavljač kolumna C" rows={4} cols={20} className="full padd" onChange={e => console.log(e.target.value)} />
+      </div>
     </div >
   );
 }
