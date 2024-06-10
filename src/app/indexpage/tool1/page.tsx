b58648a06c6a500f22b27bb20114e4bc72e752fa
ikiK <kiki.ikik@gmail.com>
2024-06-10 20:10:38 +0200

    added reex for spliting comuls A

--- src/app/indexpage/tool1/page.tsx
+++ src/app/indexpage/tool1/page.tsx
@@ -4,11 +4,18 @@ import TextareaAutosize from 'react-textarea-autosize';
 
 export default function Page() {
 
+  const columns = ["Kupac kolumna A", "Kupac kolumna B", "Kupac kolumna C", "Dobavljač kolumna A", "Dobavljač kolumna B", "Dobavljač kolumna C"]
+
   let gettingValue = function gettingValue(data: string, placeholder: string) {
-    console.log(data)
+    // console.log(data)
     let arr = data.split("\n")
     console.log(arr)
-    console.log(placeholder)
+    // console.log(placeholder)
+    arr.forEach((e, index) => {
+      arr[index] = e.replace(/GH\d*-/gi, '').split(/\W+/).join(" ");
+      console.log(e)
+    })
+    console.log(arr)
 
   }
 
@@ -19,12 +26,9 @@ export default function Page() {
         <Link href="/indexpage">NAZAD</Link><br />
       </div>
       <div className="fullFlexContainer">
-        <TextareaAutosize placeholder="Kupac kolumna A" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value, e.target.placeholder)} />
-        <TextareaAutosize placeholder="Kupac kolumna B" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value, e.target.placeholder)} />
-        <TextareaAutosize placeholder="Kupac kolumna c" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value, e.target.placeholder)} />
-        <TextareaAutosize placeholder="Dobavljač kolumna A" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value, e.target.placeholder)} />
-        <TextareaAutosize placeholder="Dobavljač kolumna B" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value, e.target.placeholder)} />
-        <TextareaAutosize placeholder="Dobavljač kolumna C" rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value, e.target.placeholder)} />
+        {columns.map((col, index) =>
+          <TextareaAutosize placeholder={col} key={index} rows={4} cols={20} className="full padd" onChange={(e) => gettingValue(e.target.value, e.target.placeholder)} />
+        )}
       </div>
     </div >
   );
